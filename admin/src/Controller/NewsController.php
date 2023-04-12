ca0b682a813dc5f6e2319975788a1b83ed037ac4
DeesreX <deesrex@gmail.com>
2023-04-12 13:20:23 +0200

    Adding Editing Functionality

--- admin/src/Controller/NewsController.php
+++ admin/src/Controller/NewsController.php
@@ -1,16 +1,36 @@
 <?php
 namespace Fox5\Component\News\Administrator\Controller;
+
 use Fox5\Component\News\Administrator\Model\NewsFormModel;
 use Joomla\CMS\MVC\Controller\FormController;
 use Joomla\Input\Input;
 
 class NewsController extends FormController
 {
-    public function execute($task){
-        $input = new Input();
-        $formData = $input->get('jform', []);
-        $newsformModel = new NewsFormModel();
-        $newsformModel->saveForm(($formData));
+    protected $view_item = 'edit'; // Set the edit view as the default view for this controller.
+    public function execute($task)
+    {
+
+        switch ($task) {
+            case 'save':
+                $input = new Input();
+                $formData = $input->get('jform', []);
+                $newsformModel = new NewsFormModel();
+                $newsformModel->saveForm(($formData));
+                break;
+
+            case 'edit':
+                $input = new Input();
+                $formData = $input->get('jform', []);
+                $newsformModel = new NewsFormModel();
+                $newsformModel->saveForm(($formData));
+                break;
+
+            default:
+                # code...
+                break;
+        }
+
     }
-    
+
 }
---
+++ admin/src/View/Edit/HtmlView.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Fox5\Component\News\Administrator\View\EditForm;
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Fox5\Component\News\Administrator\Model\NewsFormModel;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * View to edit a banner.
+ *
+ * @since  1.5
+ */
+class HtmlView extends BaseHtmlView {
+    public function display($tpl = null)
+    {
+        $this->model = new NewsFormModel();
+        $this->form = $this->model->getForm();
+        // Set the toolbar
+        $this->addToolbar();
+
+        // Display the view template
+        parent::display($tpl);
+    }
+
+    protected function addToolbar()
+    {
+        // Add the title to the toolbar
+        ToolbarHelper::title('Add News Article');
+
+        
+        ToolbarHelper::save();
+        ToolbarHelper::cancel('newsform.cancel');
+    }
+}
